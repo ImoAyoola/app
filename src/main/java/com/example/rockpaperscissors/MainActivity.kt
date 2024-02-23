ec4368486df4e68e6f17d48e50eb2aaad6485ae5
IMO AYOOLA <ayoolimo000@warren.k12.in.us>
2024-02-23 13:19:06 -0500

    beginning!

--- src/main/java/com/example/rockpaperscissors/MainActivity.kt
+++ src/main/java/com/example/rockpaperscissors/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.rockpaperscissors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rockButton: Button
+    private lateinit var paperButton: Button
+    private lateinit var scissorsButton : Button
+    private lateinit var playAgain: Button
+
+    private lateinit var playerText: TextView
+    private lateinit var droidText: TextView
+    private lateinit var winText: TextView
+
+    private var gameInSession: Boolean = true
+
+    private fun checkScissorsWin() : Boolean {
+        if(playerChoice == scissors && droidChoice == paper){
+                return true
+            }
+        }
+        return false
+    }
+    private fun checkForWin() : Boolean {
+        if(()) {
+            return true
+        } else if (()){
+            return true
+        } else if (()){
+            return true
+        } else if (()){
+            return true
+        }
+    }
+
+    while(gameInSession)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+
